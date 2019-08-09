d39f3634df67e953c3675713edc3dec9361cbeea
GSkyler <49345447+GSkyler@users.noreply.github.com>
2019-08-09 14:06:50 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  consumer_key:         '',
-  consumer_secret:      '',
-  access_token:         '',
-  access_token_secret:  '',
+  consumer_key:         'FaiJq4RxvNWTGMocL9OebNKJZ',
+  consumer_secret:      '70TP9gXcq8w6Dx7FPpqsmRQwUyiKJNoJDTOJyo86QhSpodUhJ9',
+  access_token:         '1159898988788817921-VOJRv2K90HzgRBem7NLktfUkSn07ds',
+  access_token_secret:  'JSlPIdEmw4gpGW56cZsZqlA3q67A5rGUGKanLpbUa1Yzq ',
 }
 
-//seperate file of the api keys
+//separate file of the api keys
