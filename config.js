030407e10fc2493532b46865e4133ab778f5ef20
Ashay Parikh <ashayparikh22@gmail.com>
2019-08-10 13:43:01 -0500

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  consumer_key:         'FaiJq4RxvNWTGMocL9OebNKJZ',
-  consumer_secret:      '70TP9gXcq8w6Dx7FPpqsmRQwUyiKJNoJDTOJyo86QhSpodUhJ9',
-  access_token:         '1159898988788817921-VOJRv2K90HzgRBem7NLktfUkSn07ds',
-  access_token_secret:  'JSlPIdEmw4gpGW56cZsZqlA3q67A5rGUGKanLpbUa1Yzq ',
-}
-
-//separate file of the api keys
